bf718c14777f21fe646606fc3e5146dac97b7392
Samy Bouchentouf <samy.bouchentouf@etu.minesparis.psl.eu>
2026-07-01 11:09:39 +0200

    modif documents.controller

--- backend/src/controllers/documents.controller.js
+++ backend/src/controllers/documents.controller.js
@@ -1,7 +1,24 @@
+import fs from "fs";
+import path from "path";
+
 export const upload = (req, res) => {
-    console.log("Fichier reçu :", req.file);
+
+    const file = req.file;
+
+    if (!file) {
+        return res.status(400).json({ error: "Pas de fichier" });
+    }
+
+    // ✅ chemin de sauvegarde
+    const filePath = path.join("uploads", file.originalname);
+
+    // ✅ écrire le fichier sur disque
+    fs.writeFileSync(filePath, file.buffer);
+
+    console.log("📄 Fichier sauvegardé :", filePath);
 
     res.json({
-        message: "Fichier bien reçu ✅",
+        message: "Fichier sauvegardé ✅",
+        path: filePath
     });
 };
